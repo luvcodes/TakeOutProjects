d623bb31d75b9a70dc16dc7a58c7f1c34a3d11dd
Ryan Yang <ryan0028@student.monash.edu>
2024-02-09 16:13:59 +1100

    Dish page query search

--- sky_take_out/sky-take-out/sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky_take_out/sky-take-out/sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -47,5 +47,10 @@ public interface DishMapper {
     @Autofill(value = OperationType.UPDATE)
     void update(Dish dish);
 
-
+    /**
+     * 动态条件查询菜品
+     * @param dish
+     * @return
+     */
+    List<Dish> list(Dish dish);
 }
