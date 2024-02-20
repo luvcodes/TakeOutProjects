6a2eba39eeec68a6c395cdfae34fcc9e720b9f5c
Runze Yang <ryanyyyyyyy@163.com>
2024-02-21 00:31:43 +1100

    Spring task for implement order status timing operation

--- sky_take_out/sky-take-out/sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky_take_out/sky-take-out/sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -6,6 +6,9 @@ import com.sky.entity.Orders;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 /**
  * @author ryanw
  */
@@ -37,4 +40,12 @@ public interface OrderMapper {
      */
     @Select("select * from orders where id = #{id}")
     Orders getById(Long id);
+
+    /**
+     * 根据状态和下单时间查询订单
+     * @param status
+     * @param orderTime
+     */
+    @Select("select * from orders where status = #{status} and order_time < #{orderTime}")
+    List<Orders> getByStatusAndOrdertimeLT(Integer status, LocalDateTime orderTime);
 }
