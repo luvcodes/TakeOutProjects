2b1856465460ab548f0851d9e54b99bfda9e4e09
Runze Yang <ryanyyyyyyy@163.com>
2024-02-21 14:36:39 +1100

    Top 100 sales ranking functionality implementation

--- sky_take_out/sky-take-out/sky-server/src/main/java/com/sky/service/ReportService.java
+++ sky_take_out/sky-take-out/sky-server/src/main/java/com/sky/service/ReportService.java
@@ -1,6 +1,7 @@
 package com.sky.service;
 
 import com.sky.vo.OrderReportVO;
+import com.sky.vo.SalesTop10ReportVO;
 import com.sky.vo.TurnoverReportVO;
 import com.sky.vo.UserReportVO;
 
@@ -27,4 +28,9 @@ public interface ReportService {
      * 根据时间区间统计订单数量
      */
     OrderReportVO getOrderStatistics(LocalDate begin, LocalDate end);
+
+    /**
+     * 查询指定时间区间内的销量排名top10
+     */
+    SalesTop10ReportVO getSalesTop10(LocalDate begin, LocalDate end);
 }
